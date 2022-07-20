8afe7c20f488ce2acd656b0a304383172f006e4c
lucas <lucasregoaraujo@gmail.com>
2022-07-20 16:08:00 -0300

    created AuthorsController, get AllAuthors and getAuthorById

---
+++ CourseLibrary.API/Controllers/AuthorsController.cs
@@ -0,0 +1,33 @@
+﻿using CourseLibrary.API.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace CourseLibrary.API.Controllers
+{
+    [ApiController]
+    [Route("api/authors")]
+    public class AuthorsController : ControllerBase
+    {
+
+        private readonly ICourseLibraryRepository _courseLibraryRepository;
+        public AuthorsController(ICourseLibraryRepository courseLibraryRepository)
+        {
+            _courseLibraryRepository = courseLibraryRepository ?? throw new ArgumentNullException(nameof(courseLibraryRepository));
+        }
+
+        [HttpGet()]
+        public IActionResult GetAuthors()
+        {
+            var authors = _courseLibraryRepository.GetAuthors();
+            return new JsonResult(authors);
+        }
+
+        [HttpGet("{authorId}")] // or [HttpGet("{authorId:guid}")] Os dois pontos seguidos do tipo (guid) é para eliminar ambiguidades, ou seja, só irá aceitar objetos que podem ser convertidos para o tipo gui
+        public IActionResult GetAuthor(Guid authorId)
+        {
+            var author = _courseLibraryRepository.GetAuthor(authorId);
+            if(author == null) return NotFound();
+            return new JsonResult(author);
+        }
+    }
+}
